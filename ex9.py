82173593c99dce81e7e103f4b0eda97e02b5792f
ghspl <1274471576@qq.com>
2016-09-14 11:14:10 +0800

    添加ex9

---
+++ ex9.py
@@ -0,0 +1,12 @@
+# Here's some new strande stuff, remember type it exactly.
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nAug"
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something goning on here.
+With the three double-quotes.
+we'll be able to type as much as we like
+"""
+
